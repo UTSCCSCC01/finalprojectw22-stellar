8190f5a47ee6b6b5240b7726d9b2c4ce07e12d35
Sib-K <67606843+Sib-K@users.noreply.github.com>
2022-03-03 23:14:38 -0500

    Delete MERNjs-master Directory

--- MERNjs-master/client/store.jsx
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-
-const store = createStore(reducers, applyMiddleware(thunk, logger));
-
-export default store;
